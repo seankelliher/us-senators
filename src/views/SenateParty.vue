fa62fb19ab73daaede619021bd970796997c9146
Sean Kelliher <sbkelliher@icloud.com>
2022-01-02 14:06:51 -0500

    Add 'views/SenateParty.vue' file.

---
+++ src/views/SenateParty.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <h2>By Party</h2>
+        <SenatorCards
+            msg="Placeholder for Senators Cards, displayed by party"
+        />
+    </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import SenatorCards from "@/components/SenatorCards.vue";
+
+export default {
+    name: "SenateParty",
+    components: {
+        SenatorCards,
+    },
+};
+</script>
