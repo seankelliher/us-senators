b23dd198c8e0c3a778827b030c087a7d1bec4274
Sean Kelliher <sbkelliher@icloud.com>
2022-01-02 14:06:39 -0500

    Add 'views/SenateClass.vue' file.

---
+++ src/views/SenateClass.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <h2>By Class</h2>
+        <SenatorCards
+            msg="Placeholder for Senators Cards, displayed by class"
+        />
+    </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import SenatorCards from "@/components/SenatorCards.vue";
+
+export default {
+    name: "SenateClass",
+    components: {
+        SenatorCards,
+    },
+};
+</script>
