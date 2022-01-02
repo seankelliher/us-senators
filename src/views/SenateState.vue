ae49180afa5924c7d70a67063072be32dc87ab63
Sean Kelliher <sbkelliher@icloud.com>
2022-01-02 14:06:56 -0500

    Add 'views/SenateState.vue' file.

---
+++ src/views/SenateState.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <h2>By State</h2>
+        <SenatorCards
+            msg="Placeholder for Senators Cards, displayed by state"
+        />
+    </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import SenatorCards from "@/components/SenatorCards.vue";
+
+export default {
+    name: "SenateState",
+    components: {
+        SenatorCards,
+    },
+};
+</script>
