eeae37e4271a5f63927127c6bd4202907fee8b70
Sean Kelliher <sbkelliher@icloud.com>
2022-01-02 14:02:09 -0500

    Add 'SenatorCards.vue' file (previously 'HelloWorld.vue). Placeholder at this point.

---
+++ src/components/SenatorCards.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card-gallery">
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SenatorCards",
+    props: {
+        msg: String,
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card-gallery {
+    background: #fafafa; /*grey 50*/
+}
+</style>
