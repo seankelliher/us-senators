65022da9c8e07fc903364ce8fc11d44fd98977dc
Sean Kelliher <sbkelliher@icloud.com>
2022-01-08 21:17:14 -0500

    Add 'getClasses' to computed in <script> and (new) 'senator-card' class in template.

--- src/views/SenateClass.vue
+++ src/views/SenateClass.vue
@@ -2,11 +2,27 @@
     <section>
         <h2>By Class</h2>
         <p>Placeholder for Senators Cards, displayed by class</p>
+
+        <div
+            class="senator-card"
+            v-for="(getClass, index) in getClasses"
+            :key="index.getClass"
+        >
+            {{ getClass.fName }}
+            {{ getClass.lName }}
+            {{ getClass.sClass }}
+        </div>
     </section>
 </template>
 
 <script>
 export default {
     name: "SenateClass",
+    components: {},
+    computed: {
+        getClasses() {
+            return this.$store.getters.getClasses;
+        },
+    },
 };
 </script>
