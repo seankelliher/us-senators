d6bc775b80cf0d60d1b000fcd7ad4b22a17592d4
Sean Kelliher <sbkelliher@icloud.com>
2022-01-08 22:42:38 -0500

    Add 'getStates' to computed in <script> and (new) 'senator-card' class in template.

--- src/views/SenateState.vue
+++ src/views/SenateState.vue
@@ -2,11 +2,27 @@
     <section>
         <h2>By State</h2>
         <p>Placeholder for Senators Cards, displayed by state</p>
+
+        <div
+            class="senator-card"
+            v-for="(getState, index) in getStates"
+            :key="index.getClass"
+        >
+            {{ getState.fName }}
+            {{ getState.lName }}
+            {{ getState.hState }}
+        </div>
     </section>
 </template>
 
 <script>
 export default {
     name: "SenateState",
+    components: {},
+    computed: {
+        getStates() {
+            return this.$store.getters.getStates;
+        },
+    },
 };
 </script>
