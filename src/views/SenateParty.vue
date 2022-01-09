9a5b2c05b453efc076e7a4ce97f0277dbe6f3029
Sean Kelliher <sbkelliher@icloud.com>
2022-01-08 22:42:31 -0500

    Add 'getParties' to computed in <script> and (new) 'senator-card' class in template.

--- src/views/SenateParty.vue
+++ src/views/SenateParty.vue
@@ -2,11 +2,27 @@
     <section>
         <h2>By Party</h2>
         <p>Placeholder for Senators Cards, displayed by party</p>
+
+        <div
+            class="senator-card"
+            v-for="(getParty, index) in getParties"
+            :key="index.getClass"
+        >
+            {{ getParty.fName }}
+            {{ getParty.lName }}
+            {{ getParty.sParty }}
+        </div>
     </section>
 </template>
 
 <script>
 export default {
     name: "SenateParty",
+    components: {},
+    computed: {
+        getParties() {
+            return this.$store.getters.getParties;
+        },
+    },
 };
 </script>
