4aa4ba832dd540ec34564d73cfbb29b7cbfe1e45
Sean Kelliher <sbkelliher@icloud.com>
2022-01-08 22:42:24 -0500

    Add 'getLeaders' to computed in <script> and (new) 'senator-card' class in template.

--- src/views/SenateLeaders.vue
+++ src/views/SenateLeaders.vue
@@ -2,12 +2,27 @@
     <section>
         <h2>By Leaders</h2>
         <p>Placeholder for Senators Cards, displayed by leaders</p>
+
+        <div
+            class="senator-card"
+            v-for="(getLeader, index) in getLeaders"
+            :key="index.getLeader"
+        >
+            {{ getLeader.fName }}
+            {{ getLeader.lName }}
+            {{ getLeader.lPosition }}
+        </div>
     </section>
 </template>
 
 <script>
 export default {
     name: "SenateLeaders",
+    components: {},
+    computed: {
+        getLeaders() {
+            return this.$store.getters.getLeaders;
+        },
+    },
 };
 </script>
-
