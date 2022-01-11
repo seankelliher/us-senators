45d15600b639d84ee7f9db6b8ff10c8d50f7ccba
Sean Kelliher <sbkelliher@icloud.com>
2022-01-10 21:11:58 -0500

    Add 'getParties' to computed and template.

--- src/views/SenateParty.vue
+++ src/views/SenateParty.vue
@@ -1,17 +1,37 @@
 <template>
     <section>
         <div class="gallery-title">
-            <h2>By Party</h2>
-            <p>Placeholder for Senators Cards, displayed by party</p>
+            <h3>Democrats</h3>
         </div>
-
         <div
             class="senator-card"
-            v-for="(getParty, index) in getParties"
-            :key="index.getClass"
+            v-for="(getParty, index) in getParties.democrats"
+            :key="index"
         >
-            <p>{{ getParty.fullName }}</p>
-            <p>{{ getParty.sParty }}</p>
+            <p>{{ getParty.firstName + " " + getParty.lastName }}</p>
+            <p>{{ getParty.party }}</p>
+        </div>
+        <div class="gallery-title">
+            <h3>Independents</h3>
+        </div>
+        <div
+            class="senator-card"
+            v-for="(getParty, index) in getParties.indendents"
+            :key="index"
+        >
+            <p>{{ getParty.firstName + " " + getParty.lastName }}</p>
+            <p>{{ getParty.party }}</p>
+        </div>
+        <div class="gallery-title">
+            <h3>Republicans</h3>
+        </div>
+        <div
+            class="senator-card"
+            v-for="(getParty, index) in getParties.republicans"
+            :key="index"
+        >
+            <p>{{ getParty.firstName + " " + getParty.lastName }}</p>
+            <p>{{ getParty.party }}</p>
         </div>
     </section>
 </template>
@@ -22,7 +42,11 @@ export default {
     components: {},
     computed: {
         getParties() {
-            return this.$store.getters.getParties;
+            return {
+                democrats: this.$store.getters.getDemocrats,
+                independents: this.$store.getters.getIndependents,
+                republicans: this.$store.getters.getRepublicans,
+            };
         },
     },
 };
