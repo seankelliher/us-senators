473a4191d86eea98ea448dfb602e74591fbe1e35
Sean Kelliher <sbkelliher@icloud.com>
2022-01-14 21:37:42 -0500

    Add 'InfoPanel.vue' component. Users click on 'i' icon for more information.

---
+++ src/components/InfoPanel.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="info-panel">
+        <h3
+            class="cursor-pointer"
+            v-on:click="seen === true ? (seen = false) : (seen = true)"
+        >
+            {{ heading }} <information-icon fillColor="#f44336" />
+        </h3>
+        <p v-show="seen">{{ msg1 }}</p>
+        <p v-show="seen">{{ msg2 }}</p>
+    </div>
+</template>
+
+<script>
+import InformationIcon from "vue-material-design-icons/Information.vue";
+
+export default {
+    name: "InfoPanel",
+    props: {
+        heading: String,
+        msg1: String,
+        msg2: String,
+    },
+    data() {
+        return {
+            seen: false,
+        };
+    },
+    components: {
+        InformationIcon,
+    },
+};
+</script>
