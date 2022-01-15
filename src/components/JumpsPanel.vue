c3ea2a7abcb7058adce2cab60b554a4b3e0385b3
Sean Kelliher <sbkelliher@icloud.com>
2022-01-15 13:38:10 -0500

    Add 'JumpsPanel' component. Houses 'jump links' for page.

---
+++ src/components/JumpsPanel.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="jumps-panel">
+        <p>
+            Go to:&nbsp;
+            <a v-bind:href="link1">{{ name1 }}</a>
+            &nbsp;
+            <a v-bind:href="link2">{{ name2 }}</a>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "JumpsPanel",
+    props: {
+        link1: String,
+        link2: String,
+        name1: String,
+        name2: String,
+    },
+    data() {
+        return {};
+    },
+    components: {},
+};
+</script>
