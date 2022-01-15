bfe07666dc81a39ec4f22dc63615b235f26fe5d6
Sean Kelliher <sbkelliher@icloud.com>
2022-01-15 13:42:22 -0500

    Add ids to 'gallery-title' divs. Add 'JumpsPanel' divs. Make Class I, Class II, Class III each its own 'section'.

--- src/views/SenateClass.vue
+++ src/views/SenateClass.vue
@@ -1,10 +1,18 @@
 <template>
     <section>
-        <div class="gallery-title">
+        <div class="gallery-title" id="class1">
             <InfoPanel
                 heading="Class I"
                 msg1="A class is a grouping of senators who are up for election in the same year. Article I, section 3 of the Constitution divides senators into three classes, Class I, Class II, and Class III. Each class is elected two years apart."
                 msg2="Class I terms run from the beginning of the 116th Congress on January 3, 2019, to the end of the 118th Congress on January 3, 2025. Senators in Class I were elected to office in the November 2018 general election, unless they took their seat through appointment or special election."
+            />
+        </div>
+        <div class="jumps-panel">
+            <JumpsPanel
+                link1="#class2"
+                name1="Class II"
+                link2="#class3"
+                name2="Class III"
             />
         </div>
         <div
@@ -31,10 +39,20 @@
                 </figcaption>
             </figure>
         </div>
-        <div class="gallery-title">
+    </section>
+    <section>
+        <div class="gallery-title" id="class2">
             <InfoPanel
                 heading="Class II"
                 msg1="Class II terms run from the beginning of the 117th Congress on January 3, 2021, to the end of the 119th Congress on January 3, 2027. Senators in Class II were elected to office in the November 2020 general election, unless they took their seat through appointment or special election."
+            />
+        </div>
+        <div class="jumps-panel">
+            <JumpsPanel
+                link1="#class1"
+                name1="Class I"
+                link2="#class3"
+                name2="Class III"
             />
         </div>
         <div
@@ -61,10 +79,20 @@
                 </figcaption>
             </figure>
         </div>
-        <div class="gallery-title">
+    </section>
+    <section>
+        <div class="gallery-title" id="class3">
             <InfoPanel
                 heading="Class III"
                 msg1="Class III terms run from the beginning of the 115th Congress on January 3, 2017, to the end of the 117th Congress on January 3, 2023. Senators in Class III were elected to office in the November 2016 general election, unless they took their seat through appointment or special election."
+            />
+        </div>
+        <div class="jumps-panel">
+            <JumpsPanel
+                link1="#class1"
+                name1="Class I"
+                link2="#class2"
+                name2="Class II"
             />
         </div>
         <div
@@ -95,10 +123,12 @@
 </template>
 
 <script>
+import JumpsPanel from "@/components/JumpsPanel.vue";
 import InfoPanel from "@/components/InfoPanel.vue";
 export default {
     name: "SenateClass",
     components: {
+        JumpsPanel,
         InfoPanel,
     },
     computed: {
