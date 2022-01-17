8d270697ca979a1490832f71b91507d24eee0061
Sean Kelliher <sbkelliher@icloud.com>
2022-01-16 21:42:37 -0500

    Add 'StatsPanel' component.

---
+++ src/components/StatsPanel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="stats-panel">
+        <p>
+            Population: {{ stat1 }}. Rank: {{ stat2 }}. House members:
+            {{ stat3 }}.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StatsPanel",
+    props: {
+        stat1: Number,
+        stat2: Number,
+        stat3: Number,
+    },
+    data() {
+        return {};
+    },
+    components: {},
+};
+</script>
