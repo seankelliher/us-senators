111c9f8a817998c8e9ce110f41d911c31329743a
Sean Kelliher <sbkelliher@icloud.com>
2022-01-18 22:32:49 -0500

    Remove <div class='jumps-panel'>. Not needed. It's already in the 'JumpsPanel' component.

--- src/views/SenateParty.vue
+++ src/views/SenateParty.vue
@@ -3,14 +3,12 @@
         <div class="gallery-title" id="democrats">
             <h3>Democrats</h3>
         </div>
-        <div class="jumps-panel">
-            <JumpsPanel
-                link1="#independents"
-                name1="Independents"
-                link2="#republicans"
-                name2="Republicans"
-            />
-        </div>
+        <JumpsPanel
+            link1="#independents"
+            name1="Independents"
+            link2="#republicans"
+            name2="Republicans"
+        />
         <div
             class="senator-card"
             v-for="getParty in getParties.democrats"
@@ -40,14 +38,12 @@
         <div class="gallery-title" id="independents">
             <h3>Independents</h3>
         </div>
-        <div class="jumps-panel">
-            <JumpsPanel
-                link1="#democrats"
-                name1="Democrats"
-                link2="#republicans"
-                name2="Republicans"
-            />
-        </div>
+        <JumpsPanel
+            link1="#democrats"
+            name1="Democrats"
+            link2="#republicans"
+            name2="Republicans"
+        />
         <div
             class="senator-card"
             v-for="getParty in getParties.independents"
@@ -77,14 +73,12 @@
         <div class="gallery-title" id="republicans">
             <h3>Republicans</h3>
         </div>
-        <div class="jumps-panel">
-            <JumpsPanel
-                link1="#democrats"
-                name1="Democrats"
-                link2="#independents"
-                name2="Independents"
-            />
-        </div>
+        <JumpsPanel
+            link1="#democrats"
+            name1="Democrats"
+            link2="#independents"
+            name2="Independents"
+        />
         <div
             class="senator-card"
             v-for="getParty in getParties.republicans"
