0f15d05cf17c4287dab20cbe72f7328163bb1f59
Sean Kelliher <sbkelliher@icloud.com>
2022-01-22 13:30:59 -0500

    Add 'JumpsPanelO.vue', really renaming 'JumpsPanel'.

---
+++ src/components/JumpsPanelO.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="jumps-panel">
+        <p>
+            <!--Go to:&nbsp;-->
+            <a v-bind:href="link1">{{ name1 }}</a>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <a v-bind:href="link2">{{ name2 }}</a>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <a v-bind:href="link3">{{ name3 }}</a>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "JumpsPanelO",
+    props: {
+        link1: String,
+        link2: String,
+        link3: String,
+        name1: String,
+        name2: String,
+        name3: String,
+    },
+    data() {
+        return {};
+    },
+    components: {},
+};
+</script>
