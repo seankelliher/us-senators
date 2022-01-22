0be211005d3a6144570acca8b4607c08f8d93c55
Sean Kelliher <sbkelliher@icloud.com>
2022-01-22 13:32:08 -0500

    Add 'JumpsPanelS.vue'. This is the 'jumps panel' used with 'SenateState' view.

---
+++ src/components/JumpsPanelS.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="jumps-panel">
+        <p>
+            <!--Go to:&nbsp;-->
+            <a v-bind:href="link01">{{ name01 }}</a>
+            &nbsp;
+            <a v-bind:href="link02">{{ name02 }}</a>
+            &nbsp;
+            <a v-bind:href="link03">{{ name03 }}</a>
+            &nbsp;
+            <a v-bind:href="link04">{{ name04 }}</a>
+            &nbsp;
+            <a v-bind:href="link05">{{ name05 }}</a>
+            &nbsp;
+            <a v-bind:href="link06">{{ name06 }}</a>
+            &nbsp;
+            <a v-bind:href="link07">{{ name07 }}</a>
+            &nbsp;
+            <a v-bind:href="link08">{{ name08 }}</a>
+            &nbsp;
+            <a v-bind:href="link09">{{ name09 }}</a>
+            &nbsp;
+            <a v-bind:href="link10">{{ name10 }}</a>
+            &nbsp;
+            <a v-bind:href="link11">{{ name11 }}</a>
+            &nbsp;
+            <a v-bind:href="link12">{{ name12 }}</a>
+            &nbsp;
+            <a v-bind:href="link13">{{ name13 }}</a>
+            &nbsp;
+            <a v-bind:href="link14">{{ name14 }}</a>
+            &nbsp;
+            <a v-bind:href="link15">{{ name15 }}</a>
+            &nbsp;
+            <a v-bind:href="link16">{{ name16 }}</a>
+            &nbsp;
+            <a v-bind:href="link17">{{ name17 }}</a>
+            &nbsp;
+            <a v-bind:href="link18">{{ name18 }}</a>
+            &nbsp;
+            <a v-bind:href="link19">{{ name19 }}</a>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "JumpsPanel",
+    props: {
+        link01: String,
+        link02: String,
+        link03: String,
+        link04: String,
+        link05: String,
+        link06: String,
+        link07: String,
+        link08: String,
+        link09: String,
+        link10: String,
+        link11: String,
+        link12: String,
+        link13: String,
+        link14: String,
+        link15: String,
+        link16: String,
+        link17: String,
+        link18: String,
+        link19: String,
+        name01: String,
+        name02: String,
+        name03: String,
+        name04: String,
+        name05: String,
+        name06: String,
+        name07: String,
+        name08: String,
+        name09: String,
+        name10: String,
+        name11: String,
+        name12: String,
+        name13: String,
+        name14: String,
+        name15: String,
+        name16: String,
+        name17: String,
+        name18: String,
+        name19: String,
+    },
+    data() {
+        return {};
+    },
+    components: {},
+};
+</script>
