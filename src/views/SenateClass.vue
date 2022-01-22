2c6b5d93a9d59455b48de0069fad6eecad868374
Sean Kelliher <sbkelliher@icloud.com>
2022-01-22 13:37:12 -0500

    Remove 'JumpsPanels' in each section. Add one 'JumpsPanelO' to bottom of page.

--- src/views/SenateClass.vue
+++ src/views/SenateClass.vue
@@ -8,12 +8,6 @@
             msg1="A class is a grouping of senators who are up for election in the same year. Article I, section 3 of the Constitution divides senators into three classes, Class I, Class II, and Class III. Each class is elected two years apart."
             msg2="Class I terms run from the beginning of the 116th Congress on January 3, 2019, to the end of the 118th Congress on January 3, 2025. Senators in Class I were elected to office in the November 2018 general election, unless they took their seat through appointment or special election."
         />
-        <JumpsPanel
-            link1="#class2"
-            name1="Class II"
-            link2="#class3"
-            name2="Class III"
-        />
         <div
             class="senator-card"
             v-for="getClass in getClasses.classOne"
@@ -46,12 +40,6 @@
         <InfoPanel
             heading="What is Class II?"
             msg1="Class II terms run from the beginning of the 117th Congress on January 3, 2021, to the end of the 119th Congress on January 3, 2027. Senators in Class II were elected to office in the November 2020 general election, unless they took their seat through appointment or special election."
-        />
-        <JumpsPanel
-            link1="#class1"
-            name1="Class I"
-            link2="#class3"
-            name2="Class III"
         />
         <div
             class="senator-card"
@@ -86,12 +74,6 @@
             heading="What is Class III?"
             msg1="Class III terms run from the beginning of the 115th Congress on January 3, 2017, to the end of the 117th Congress on January 3, 2023. Senators in Class III were elected to office in the November 2016 general election, unless they took their seat through appointment or special election."
         />
-        <JumpsPanel
-            link1="#class1"
-            name1="Class I"
-            link2="#class2"
-            name2="Class II"
-        />
         <div
             class="senator-card"
             v-for="getClass in getClasses.classThree"
@@ -117,15 +99,23 @@
             </figure>
         </div>
     </section>
+    <JumpsPanelO
+        link1="#class1"
+        name1="Class I"
+        link2="#class2"
+        name2="Class II"
+        link3="#class3"
+        name3="Class III"
+    />
 </template>
 
 <script>
-import JumpsPanel from "@/components/JumpsPanel.vue";
+import JumpsPanelO from "@/components/JumpsPanelO.vue";
 import InfoPanel from "@/components/InfoPanel.vue";
 export default {
     name: "SenateClass",
     components: {
-        JumpsPanel,
+        JumpsPanelO,
         InfoPanel,
     },
     computed: {
