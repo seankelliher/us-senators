c2cbc1ac80bafb6fcdcf9c79e83ef424ceedcbbd
Sean Kelliher <sbkelliher@icloud.com>
2022-02-09 12:50:49 -0500

    Add 'src/views/SenateDetail.vue' view. This is to show details on each Senator.

---
+++ src/views/SenateDetail.vue
@@ -0,0 +1,46 @@
+<template>
+    <section>
+        <div class="gallery-title">
+            <h2>
+                <!--Senator {{ this.$route.params.firstName }}
+                {{ this.$route.params.lastName }}-->
+            </h2>
+        </div>
+        <div
+            class="senator-card"
+            v-for="getDetail in getDetails.senatorDetails"
+            :key="getDetail.bioguideId"
+        >
+            <figure>
+                <img
+                    :src="require(`../assets/images/${getDetail.portrait}.jpg`)"
+                    :alt="getDetail.firstName + ' ' + getDetail.lastName"
+                />
+                <figcaption>
+                    {{ getDetail.firstName + " " + getDetail.lastName }}
+                    <br />
+                    {{ getDetail.state }}
+                </figcaption>
+            </figure>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "SenateDetail",
+    data() {
+        return {};
+    },
+    components: {},
+    computed: {
+        getDetails() {
+            return {
+                senatorDetails: this.$store.getters.getSenatorDetails(
+                    this.$route.params.portrait
+                ),
+            };
+        },
+    },
+};
+</script>
