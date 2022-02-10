d70ced02c85495379eed9fd3c91c87b7d17932f4
Sean Kelliher <sbkelliher@icloud.com>
2022-02-10 16:11:42 -0500

    Finish building 'SenateDetail' view.

--- src/views/SenateDetail.vue
+++ src/views/SenateDetail.vue
@@ -1,27 +1,61 @@
 <template>
     <section>
-        <div class="gallery-title">
-            <h2>
-                <!--Senator {{ this.$route.params.firstName }}
-                {{ this.$route.params.lastName }}-->
-            </h2>
-        </div>
         <div
-            class="senator-card"
+            class="senator-card-detail"
             v-for="getDetail in getDetails.senatorDetails"
             :key="getDetail.bioguideId"
         >
+            <h2>{{ getDetail.firstName + " " + getDetail.lastName }}</h2>
             <figure>
                 <img
                     :src="require(`../assets/images/${getDetail.portrait}.jpg`)"
                     :alt="getDetail.firstName + ' ' + getDetail.lastName"
                 />
-                <figcaption>
-                    {{ getDetail.firstName + " " + getDetail.lastName }}
-                    <br />
-                    {{ getDetail.state }}
-                </figcaption>
             </figure>
+            <p>
+                <strong>State:</strong> <br />
+                {{ getDetail.state }}
+            </p>
+            <p v-if="getDetail.party === 'D'">
+                <strong>Political Party:</strong> <br />Democratic
+            </p>
+            <p v-if="getDetail.party === 'I'">
+                <strong>Political Party:</strong> <br />Independent
+            </p>
+            <p v-if="getDetail.party === 'R'">
+                <strong>Political Party:</strong> <br />Republican
+            </p>
+            <p>
+                <strong>Senate Class:</strong> <br />{{ getDetail.senateClass }}
+            </p>
+            <p>
+                <strong>Assumed Office:</strong> <br />
+                {{ getDetail.assumedOffice }}
+            </p>
+            <p v-if="getDetail.leadershipPosition === null">
+                <strong>Leadership Position:</strong> <br />none
+            </p>
+            <p v-if="getDetail.leadershipPosition !== null">
+                <strong>Leadership Position:</strong> <br />
+                {{ getDetail.leadershipPosition }}
+            </p>
+            <p>
+                <strong>Office Address:</strong> <br />{{ getDetail.address1
+                }}<br />{{ getDetail.address2 }}
+            </p>
+            <p><strong>Telephone:</strong> <br />{{ getDetail.phone }}</p>
+            <p>
+                <strong>Website:</strong> <br />
+                <a :href="`https://www.${getDetail.website}`" target="_blank">
+                    {{ getDetail.website }}
+                </a>
+            </p>
+            <p>
+                <strong>Contact Form:</strong> <br />
+                <a :href="`https://www.${getDetail.email}`" target="_blank">
+                    {{ getDetail.email }}
+                </a>
+            </p>
         </div>
     </section>
 </template>
@@ -41,6 +75,9 @@ export default {
                 ),
             };
         },
+    },
+    mounted() {
+        window.scrollTo(0, 0);
     },
 };
 </script>
