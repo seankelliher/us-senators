08aab1073fd71357fe3ca48dc378ce6e47fc70a1
Sean Kelliher <sbkelliher@icloud.com>
2022-04-15 22:29:37 -0400

    Add the component SenatorCard2.vue. Has photo, name, party, state.

---
+++ src/components/SenatorCard2.vue
@@ -0,0 +1,37 @@
+<template>
+    <div
+        class="senator-card"
+        v-for="detail in details"
+        :key="detail.bioguideId"
+    >
+        <figure>
+            <img
+                :src="require(`../assets/images/${detail.portrait}.jpg`)"
+                :alt="detail.firstName + ' ' + detail.lastName"
+            />
+            <figcaption>
+                <a :href="`detail/${detail.portrait}`">
+                    {{
+                        detail.firstName +
+                        " " +
+                        detail.lastName +
+                        " (" +
+                        detail.party +
+                        ")"
+                    }}
+                    <br />
+                    {{ detail.state }}
+                </a>
+            </figcaption>
+        </figure>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SenatorCard2",
+    props: {
+        details: Object,
+    },
+};
+</script>
