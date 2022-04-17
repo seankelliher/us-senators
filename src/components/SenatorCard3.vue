375690837acd24bb0790be33f5b12a3eafe057fa
Sean Kelliher <sbkelliher@icloud.com>
2022-04-16 21:01:23 -0400

    Add SenateCard3.vue, used for Leaders view.

---
+++ src/components/SenatorCard3.vue
@@ -0,0 +1,41 @@
+<!--SenatorCard3: used for Leaders.-->
+<!--Contains photo, first name, last name, party, state, leadership role.-->
+<template>
+    <div
+        class="senator-card"
+        v-for="detail in details"
+        :key="detail.bioguideId"
+    >
+        <figure>
+            <img
+                :src="require(`../assets/images/${detail.portrait}.jpg`)"
+                :alt="detail.firstName + ' ' + detail.lastName"
+            />
+            <figcaption>
+                <a :href="`detail/${detail.portrait}`">
+                    {{
+                        detail.firstName +
+                        " " +
+                        detail.lastName +
+                        " (" +
+                        detail.party +
+                        ")"
+                    }}
+                    <br />
+                    {{ detail.state }}
+                    <br />
+                    {{ detail.leadershipPosition }}
+                </a>
+            </figcaption>
+        </figure>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SenatorCard2",
+    props: {
+        details: Object,
+    },
+};
+</script>
