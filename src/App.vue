0a53f66d2a2088ab39c08fd491ce8e04946d5c67
Sean Kelliher <sbkelliher@icloud.com>
2022-06-25 11:22:45 -0400

    Modify formating to meet ES Lint requirements.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
     <header>
-        <h1><router-link to="/">United States Senate</router-link></h1>
+        <h1>
+            <router-link to="/">
+                United States Senate
+            </router-link>
+        </h1>
         <p>A guide to Senators in the 117th Congress</p>
     </header>
     <nav>
-        <button><router-link to="/">State</router-link></button>
-        <button><router-link to="/class">Class</router-link></button>
-        <button><router-link to="/party">Party</router-link></button>
-        <button><router-link to="/leaders">Leaders</router-link></button>
+        <button>
+            <router-link to="/">
+                State
+            </router-link>
+        </button>
+        <button>
+            <router-link to="/class">
+                Class
+            </router-link>
+        </button>
+        <button>
+            <router-link to="/party">
+                Party
+            </router-link>
+        </button>
+        <button>
+            <router-link to="/leaders">
+                Leaders
+            </router-link>
+        </button>
     </nav>
     <router-view />
 </template>
