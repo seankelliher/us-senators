b20ee2cdcd5e98410e090e2214a4ea73c381f691
Sean Kelliher <sbkelliher@icloud.com>
2022-06-25 11:24:46 -0400

    Revise rules for ES Lint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    extends: [
+        // add more generic rulesets here, such as:
+        // 'eslint:recommended',
+        "plugin:vue/vue3-recommended",
+        // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+    ],
+    rules: {
+        // override/add rules settings here, such as:
+        // 'vue/no-unused-vars': 'error'
+        "vue/script-indent": ["error", 4, {"baseIndent": 0}],
+        "vue/html-indent": ["error", 4, {"baseIndent": 1}],
+        "semi": [2, "always"],
+        "comma-dangle": ["error", "always-multiline"],
+    },
 };
